73cac078ce82968c9c35f525b504b8687dd658a6
tati-b127 <101515412+tati-b127@users.noreply.github.com>
2022-06-27 15:09:58 +0300

    scroll catalog, fixed modal

--- js/tabs.js
+++ js/tabs.js
@@ -8,46 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelectorAll('.catalog-preview__item').forEach(function(tabContent) {
                 tabContent.classList.remove('catalog-preview__item_active')});
             document.querySelector(`[data-target="${path}"]`).classList.add('catalog-preview__item_active');
+			document.querySelector(`[data-target="${path}"]`).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+		});
+
         })
     })
-})
-
-// MOBILE SCROLL
-
-(() => {
-	const MOBILE_WIDTH = 768;
-
-	function getWindowWidth () {
-	  return Math.max(
-	    document.body.scrollWidth,
-	    document.documentElement.scrollWidth,
-	    document.body.offsetWidth,
-	    document.documentElement.offsetWidth,
-	    document.body.clientWidth,
-	    document.documentElement.clientWidth
-	  );
-	}
-
-	function scrollToContent (link, isMobile) {
-		if (isMobile && getWindowWidth() > MOBILE_WIDTH) {
-			return;
-		}
-
-	  const href = link.getAttribute('data-path').substring(1);
-	  const scrollTarget = document.getElementsByName(href);
-	  const elementPosition = scrollTarget.getBoundingClientRect().top;
-
-	  window.scrollBy({
-	      top: elementPosition,
-	      behavior: 'smooth'
-	  });
-	}
-
-	document.querySelectorAll('.artist__btn').forEach(link => {
-	  link.addEventListener('click', function(e) {
-	      e.preventDefault();
-
-	      scrollToContent(this, true);
-	  });
-	});
-})();
