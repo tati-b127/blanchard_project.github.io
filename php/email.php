a8d947c0c7681aa91ae9ead5b6d56919df2d0d21
tati-b127 <101515412+tati-b127@users.noreply.github.com>
2022-07-04 14:25:03 +0300

    aria-label catalog

--- php/email.php
+++ php/email.php
@@ -24,7 +24,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			$message  .= "</html>";
 			// Окончание формирования тела письма
 			// Посылаем письмо
-			mail ($to, $subject,  $headers);
+			mail ($to, $subject, $headers);
 }
 else
 {
